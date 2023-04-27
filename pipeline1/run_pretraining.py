92a13c65b2ca728c20da24d1d0e10251dfd6744f
Danielle Dequin <dmdequin@gmail.com>
2023-04-27 12:27:38 +0200

    Adding pre-training using config file n_cf2_pretraining for debugging missing file in training

--- pipeline1/run_pretraining.py
+++ pipeline1/run_pretraining.py
@@ -6,4 +6,6 @@ import os
 os.system(f'CUDA_VISIBLE_DEVICES=0 python pretraining.py -C l1_pretraining')
 os.system(f'CUDA_VISIBLE_DEVICES=0 python pretraining.py -C l1b_pretraining')
 os.system(f'CUDA_VISIBLE_DEVICES=0 python pretraining.py -C l2_pretraining')
+os.system(f'CUDA_VISIBLE_DEVICES=0 python pretraining.py -C n_cf2_pretraining') # THIS WAS NOT INCLUDED, and IS PERHAPS WHY THE FILE IS NOT CREATED FOR THE TRAINING
+
 
